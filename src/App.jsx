20196fac0240ffcfb8077fb172d0da77dbcfa13c
muhammadmubeen17 <mubeenahmad1920@gmail.com>
2024-05-22 17:26:48 +0500

    added hash router for github pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+// import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import {
+  createBrowserRouter,
+  createHashRouter,
+  Navigate,
+  RouterProvider,
+} from "react-router-dom";
 import 'rsuite/dist/rsuite.min.css';
 import Home from './Components/Home';
 import Layout from './Components/Layout';
@@ -9,19 +15,39 @@ import Team from './Components/Team';
 import Type from './Components/Type';
 
 function App() {
+
+  const router = createHashRouter([
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        { path: "", element: <Home /> },
+        { path: "pokemon/:slug", element: <Pokemon /> },
+        { path: "teams", element: <Teams /> },
+        { path: "team/:slug", element: <Team /> },
+        { path: "type/:slug", element: <Type /> },
+      ],
+    },
+    {
+      path: "*",
+      element: <Navigate to="/" replace />
+    },
+  ]);
+
   return (
-    <Router>
-        <Routes>
-          <Route path='/' element={<Layout />}>
-            <Route path="" element={<Home />} />
-            <Route path='pokemon/:slug' element={<Pokemon />} />
-            <Route path="teams" element={<Teams />} />
-            <Route path="team/:slug" element={<Team />} />
-            <Route path="type/:slug" element={<Type />} />
-          </Route>
-          <Route path="*" element={<div>404 Not Found</div>} />
-        </Routes>
-    </Router>
+    // <Router>
+    //     <Routes>
+    //       <Route path='/' element={<Layout />}>
+    //         <Route path="" element={<Home />} />
+    //         <Route path='pokemon/:slug' element={<Pokemon />} />
+    //         <Route path="teams" element={<Teams />} />
+    //         <Route path="team/:slug" element={<Team />} />
+    //         <Route path="type/:slug" element={<Type />} />
+    //       </Route>
+    //       <Route path="*" element={<div>404 Not Found</div>} />
+    //     </Routes>
+    // </Router>
+    <RouterProvider router={router} />
   );
 }
 
