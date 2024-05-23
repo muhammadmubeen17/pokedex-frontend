4a8c7cb9a195d6c3d0f9dda39416443608036385
muhammadmubeen17 <mubeenahmad1920@gmail.com>
2024-05-23 13:58:33 +0500

    team card fix

--- src/Components/Teams.jsx
+++ src/Components/Teams.jsx
@@ -37,11 +37,11 @@ function Teams() {
                                 teams.length > 0 ?
                                     teams.map((team, index) => (
                                         <div key={index} className="w-full sm:w-1/2 md:w-1/3">
-                                            <div className="card p-5 bg-[#F9F9F9] rounded shadow-lg m-2 relative">
+                                            <div className="card px-5 py-8 bg-[#F9F9F9] rounded shadow-lg m-2 relative">
                                                 <Link to={`/team/${team.slug.current}`} className="absolute top-0 bottom-0 left-0 right-0"></Link>
                                                 <div className="flex flex-col justify-center items-center">
-                                                    <div className="text-center mt-5 font-bold">{team.name}</div>
-                                                    <img src={team.imageUrl} alt={team.name} width={100} />
+                                                    <div className="text-center mb-5 font-bold">{team.name}</div>
+                                                    <img src={team.imageUrl} alt={team.name} className='h-32' />
                                                 </div>
                                             </div>
                                         </div>
